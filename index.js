406a520c9fb607c7567ae5e4299fcfe4a4fb60ff
Bob Reselman <reselbob@gmail.com>
2020-07-31 22:00:01 -0700

    adjusted default port

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const connect = require('connect');
 const serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080, function(){
-    console.log('Server running on 8080...');
+connect().use(serveStatic(__dirname)).listen(9090, function(){
+    console.log('Server running on 9090...');
 });
